be2e4300fc7ffc6573eb9fba64d090a5de2fa200
Roberto Khoe <robertokhoesuroso@Robertos-Mini.fritz.box>
2026-08-22 00:13:13 +0200

    Add brew-launcher 0.1.0 formula

--- Formula/brew-launcher.rb
+++ Formula/brew-launcher.rb
@@ -2,7 +2,8 @@ class BrewLauncher < Formula
   desc "Interactive launcher for installed Homebrew CLI applications"
   homepage "https://github.com/ltdan-88/brew-launcher"
   url "https://github.com/ltdan-88/brew-launcher/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "663f8c4836b364b7e06a6d8e6c6bf8525641ceb5e9501f934070c3775f5ca2a6"
+  version "0.1.0"
   license "MIT"
 
   depends_on "fzf"
@@ -12,6 +13,7 @@ class BrewLauncher < Formula
   end
 
   test do
-    assert_match "brew-launcher", shell_output("#{bin}/brew-launcher --version")
+    assert_match "brew-launcher 0.1.0",
+      shell_output("#{bin}/brew-launcher --version")
   end
 end
